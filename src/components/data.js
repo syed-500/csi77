4fdaba7ebc39bb0f135c57c6ef071904267910b1
syed-500 <155297256+syed-500@users.noreply.github.com>
2024-09-28 08:11:03 +0530

    2

--- src/components/data.js
+++ src/components/data.js
@@ -6,7 +6,7 @@ const displayData = [
         img: "",
         title: "Hackathon AIESEC",
         date: ": 10th & 11th November 2024",
-        loc: ": CIC Lab",
+        loc: ": CIC Labs",
         para: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi consectetur, reprehenderit ut consequuntur accusantium, veniam fugit magni similique illum laborum quaerat neque commodi deleniti. Adipisci, rem explicabo. Mollitia, accusantium cumque."
     },
     {
